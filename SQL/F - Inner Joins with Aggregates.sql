eed050a338a303bb2d2ae1767fee146eadb2fa93
Dan Gilleland <dgilleland@users.noreply.github.com>
2019-10-25 08:24:51 -0600

    Update F - Inner Joins with Aggregates.sql

--- SQL/F - Inner Joins with Aggregates.sql
+++ SQL/F - Inner Joins with Aggregates.sql
@@ -5,14 +5,21 @@ GO
 --1. How many staff are there in each position? Select the number and Position Description
 SELECT  PositionDescription,                    --  <-- non-aggregate
         COUNT(S.StaffID) AS 'Number of Staff'   --  <-- aggregate
-FROM    Staff S
-    INNER JOIN Position P ON P.PositionID = S.PositionID
+FROM    Staff AS S
+    INNER JOIN Position AS P ON P.PositionID = S.PositionID
 GROUP BY PositionDescription 
  
+-- Out of curiosity, what are all the position names?
+SELECT  PositionDescription FROM Position -- There are 7 positions...
+-- but I only see 6 positions in the answer to Q1.
+-- That's because I'm using an INNER JOIN, and data has to be present
+-- in BOTH tables for it to be included.
+-- Which position does not have staff? For that answer, you'll have to wait for the OUTER JOIN.
+
 --2. Select the average mark for each course. Display the CourseName and the average mark. Sort the results by average in descending order.
 SELECT  CourseName, AVG(Mark) AS 'Average Mark'
-FROM    Registration R
-    INNER JOIN Course C ON R.CourseId = C.CourseId
+FROM    Registration AS R
+    INNER JOIN Course AS C ON R.CourseId = C.CourseId
 GROUP BY CourseName
 ORDER BY 'Average Mark' DESC
 
@@ -23,8 +30,8 @@ ORDER BY 'Average Mark' DESC
 --4. Select the average Mark for each student. Display the Student Name and their average mark. Use table aliases in your FROM & JOIN clause.
 SELECT  S.FirstName  + ' ' + S.LastName AS 'Student Name',
         AVG(R.Mark)                     AS 'Average'
-FROM    Registration R
-        INNER JOIN Student S
+FROM    Registration AS R
+        INNER JOIN Student AS S
             ON S.StudentID = R.StudentID
 GROUP BY    S.FirstName  + ' ' + S.LastName  -- Since my non-aggregate is an expression,
                                              -- I am using the same expression in my GROUP BY
@@ -32,8 +39,8 @@ GROUP BY    S.FirstName  + ' ' + S.LastName  -- Since my non-aggregate is an exp
 --5. Select the same data as question 4 but only show the student names and averages that are > 80. (HINT: Remember the HAVING clause?)
  -- TODO: Student Answer Here... 
 
- 
---6.what is the highest, lowest and average payment amount for each payment type Description? 
+
+--6. What is the highest, lowest and average payment amount for each payment type Description?
  -- TODO: Student Answer Here... 
 
  
