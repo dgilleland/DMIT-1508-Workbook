709b92e42969ccc1b00f4dbc1e0170ac755cb7c7
Dan Gilleland <dgilleland@users.noreply.github.com>
2019-10-30 08:21:22 -0600

    Update A-Insert.sql

--- DML/A-Insert.sql
+++ DML/A-Insert.sql
@@ -42,6 +42,11 @@ VALUES ('Shane', 'Bell', GETDATE(),
         (SELECT PositionID
         FROM   Position
         WHERE  PositionDescription = 'Instructor'))
+-- 2.c. We have an open position in the staff.
+SELECT  PositionDescription
+FROM    Position
+WHERE   PositionID NOT IN (SELECT PositionID FROM Staff)
+--      Add Sheldon Murray as the new Assistant Dean.
 
 -- 3. There are three additional clubs being started at the school:
 --      - START - Small Tech And Research Teams
