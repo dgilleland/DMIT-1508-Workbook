58a61bbaebe26562452ef87e4299d476933ab030
Dan Gilleland <dgilleland@users.noreply.github.com>
2020-02-25 11:31:47 -0700

    Fix - Remove answers to student practice questions

--- SQL/D - Simple Joins Exercise.sql
+++ SQL/D - Simple Joins Exercise.sql
@@ -32,9 +32,6 @@ ORDER BY 'Staff Full Name', CourseId
 
 --3.	Select all the Club ID's and the Student full names that are in them
 -- TODO: Student Answer Here...
-SELECT  ClubId, FirstName + ' ' + LastName AS 'Student Full Name'
-FROM    Activity A
-    INNER JOIN Student S ON A.StudentID = S.StudentID
 
 --4.	Select the Student full name, courseID's and marks for studentID 199899200.
 SELECT  S.FirstName + ' ' + S.LastName AS 'Student Name',
@@ -47,44 +44,18 @@ WHERE   S.StudentID = 199899200
 
 --5.	Select the Student full name, course names and marks for studentID 199899200.
 -- TODO: Student Answer Here...
-SELECT  FirstName + ' ' + LastName AS 'Student',
-        CourseName,
-        Mark
-FROM    Student S
-    INNER JOIN Registration R ON R.StudentID = S.StudentID
-    INNER JOIN Course C ON C.CourseId = R.CourseId
-WHERE   S.StudentID = '199899200'
 
 --6.	Select the CourseID, CourseNames, and the Semesters they have been taught in
 -- TODO: Student Answer Here...
-SELECT  C.CourseId, C.CourseName, R.Semester
-FROM    Course C
-    INNER JOIN Registration R ON C.CourseId = R.CourseId
 
 --7.	What Staff Full Names have taught Networking 1?
 -- TODO: Student Answer Here...
-SELECT  FirstName + ' ' + LastName AS 'Staff Name'
-FROM    Staff S
-    INNER JOIN Registration R ON S.StaffID = R.StaffID
-    INNER JOIN Course C ON R.CourseId = C.CourseId
-WHERE   CourseName = 'Networking 1'
-
 
 --8.	What is the course list for student ID 199912010 in semester 2001S. Select the Students Full Name and the CourseNames
 -- TODO: Student Answer Here...
-SELECT  FirstName + ' ' + LastName AS 'Student',
-        CourseName
---        , S.StudentID -- Used for WHERE clause
---        , R.Semester  -- Used for WHERE clause
-FROM    Student S
-    INNER JOIN Registration R ON S.StudentID = R.StudentID
-    INNER JOIN Course C       ON R.CourseId = C.CourseId
-WHERE   S.StudentID = 199912010
-  AND   R.Semester = '2001S'
 
 --9. What are the Student Names, courseID's with individual Marks at 80% or higher? Sort the results by course.
 -- TODO: Student Answer Here...
 
 --10. Modify the script from the previous question to show the Course Name along with the ID.
 -- TODO: Student Answer Here...
-
